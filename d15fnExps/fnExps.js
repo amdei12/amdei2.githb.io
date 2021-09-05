ca4904a388e163f1fd9675660bd92b3575a51abd
daniel <daniel.amdei@gmail.com>
2021-09-05 14:25:47 -0500

    bbb

--- d15fnExps/fnExps.js
+++ d15fnExps/fnExps.js
@@ -12,7 +12,8 @@ function times100(num){
   
   function myMap(func,arr){
       const newarr=new Array(arr.length);
-      for(let i=0;i<arr.length;i++){
+      let m=att.length;
+      for(let i=0;i<m;i++){
           newarr[i]=func(arr[i])
       }
       return newarr;
