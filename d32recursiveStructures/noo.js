2a169579a8f19c91cb3492e222d24a45fd77dcc8
daniel <daniel.amdei@gmail.com>
2021-09-16 20:24:51 -0500

    ss

---
+++ d32recursiveStructures/noo.js
@@ -0,0 +1,48 @@
+
+
+// let linkedList=function(a) {
+//     let count=0;
+//      this.a=a;
+//      next=null;
+
+// function add(o) {
+//     let head=null;
+//     if(head==null){
+//         head=one;
+//         count++;
+//     }
+//     else{
+//         let current=head;
+//          while(current!=null){
+//              current=current.next;
+//              count++;
+//          }
+//          current.next=o;
+//     }
+//      return o;
+//     }
+     
+//     function  get(a,b) {
+//         if(count==0){
+//             return null;
+//         }else{
+//             console.log(a(b));
+//             --b;
+        
+//         get(a,b);
+//         }
+        
+//     }
+// }
+// one=new linkedList(1);
+// two=new linkedList(2);
+// tree=new linkedList(3);
+// four=new linkedList(4);
+// list=new
+//     (linkedList.add(one));
+//     (linkedList.add(two));
+//     (linkedList.add(tree));
+//     (linkedList.add(four));
+// //console.log(linkedList.add,count);
+//let arr=[1,2,3];
+ 
