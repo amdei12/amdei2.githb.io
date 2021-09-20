2f5d372c8e55c14e797a959f72259457182ec818
daniel <daniel.amdei@gmail.com>
2021-09-19 20:14:46 -0500

    cfgg

---
+++ study/dann.js
@@ -0,0 +1,120 @@
+// peope.map(key=>key.name,key.id)
+
+
+
+// function revese(str,i){
+//     let x=" ";
+//     if(i==0){
+//         return x
+//     }
+//   return x+str.charAt(i)+revese(str,i);
+    
+// }
+// let a="daniel";
+// let b=6;
+
+// console.log(revese(a,b));
+// let group = {
+//     title: "Our Group",
+//     students: ["John", "Pete", "Alice"],
+//     showList() {
+//       this.students.forEach(function(student) {
+//         console.log(this.title + ': ' + student)
+//       });
+//     }
+//   };
+//   group.showList();
+// let group = {
+//   title: "Our Group",
+//   students: ["John", "Pete", "Alice"],
+//   showList() {
+//     this.students.forEach(student=> {
+//       console.log(this.title + ': ' + student)
+//     });
+//   }
+// };
+// group.showList();
+
+  
+// function chackArray(arr){
+//     if(Array.isArray(arr)){
+//         return true;
+//     }else 
+//     return false;
+// }
+// let a=[];
+// console.log(chackArray(a));
+
+// function clone(arr){
+//     return b=[...arr];
+// }
+// let a=[1,2,3,4,5]
+// console.log(clone(a));
+// let a=["Red", "Green", "White", "Black"];
+// function first(arr){
+   
+//     let x= a.join(" ");
+//     return x;
+// }
+// console.log(first(a));
+
+ const t ={name:"tiger",r:[4,4,3,5]};
+ const b ={name:"brysion",r:[3,4,3,4]};
+ const p ={name:"padrag",r:[3,5,4,4]};
+ const par=[4,4,3,5];
+ tor=[t,b,p]
+
+// function sxz(ar1){
+//   return ar1.map(play=>sum(play.r));
+// }
+  
+//   function sum(arr){
+//       let suma=0;
+//       for(let point of arr){
+//           suma+=point;
+//       }
+     
+//     return suma;
+//   }
+//  console.log(sxz(tor));
+
+
+// function calucnamee(arr){
+//     return arr.map(palyer=>palyer:palyer.name,valu.score:sum(valu.r));
+// }
+// function sum(arr){
+//     suma=0;
+//   for(let valu of sum){
+//       suma+=valu;
+//   }
+//   return suma;
+
+
+// }
+// console.log(calucnamee(tor));
+// 'use strict'
+// function Circle(){
+//     this.drow=function(){
+//         console.log(this);
+//     }
+// }
+// c=new Circle();
+// console.log(c.drow());
+// drow=c.drow;
+// //console.log(drow);
+// console.log(drow());
+    
+    // firstName: "John",
+    // this.sayHi=function(){
+    // console.log(`Hello, this.firstName!`);
+    // }};
+    // //c=new user();
+    // //console.log(c.sayHi());
+    // //console.log(setTimeout(function() {  user.sayHi(); }, 1000);
+    // setTimeout(() => user.sayHi, 1000);
+   
+    function diif(arr){
+        return arr.reduce((key,){
+        ((arr.findFirst(key)-+key)+arr.findFirst(key));}
+    }
+    console.log(diif(x));
---
+++ study/rss.js
@@ -0,0 +1,114 @@
+ //let x=[1,2,3,4];
+// y=x.map(key=>2*key);
+// console.log(y);
+// let m=['erec','sobra','somebdy']
+// let y=x.map(key=>key.toString());
+// let z=m.map(key=>key.toUpperCase());
+// console.log(z);
+// namesOnly=[
+//     {
+//         name: "Angelina Jolie",
+//         age: 80
+//     },
+//     {
+//         name: "Eric Jones",
+//         age: 2
+//     },
+//     {
+//         name: "Paris Hilton",
+//         age: 5
+//     },
+//     {
+//         name: "Kayne West",
+//         age: 16
+//     },
+//     {
+//         name: "Bob Ziroll",
+//         age: 100
+//     }
+// ]; 
+// let z=namesOnly.map(key=>key.age>18?key.name+" "+"can go":key.name+" "+" canot go")
+// console.log(z);
+
+// function total(arr) {
+//     return arr.reduce((key,valu)=>valu+=key,0)
+//  }
+ 
+//  console.log(total([1,2,3])); // 
+
+// function stringConcat(arr) {
+//    return arr.reduce((key,valu)=>valu+=key," ") ;
+//  }
+ 
+//  console.log(stringConcat([1,2,3]));
+// function totalVotes(arr) {
+//     return voters.reduce((key,valu)=>valu+=+(voted),0)  
+//  }
+ 
+//  var voters = [
+//      {name:'Bob' , age: 30, voted: true},
+//      {name:'Jake' , age: 32, voted: true},
+//      {name:'Kate' , age: 25, voted: false},
+//      {name:'Sam' , age: 20, voted: false},
+//      {name:'Phil' , age: 21, voted: true},
+//      {name:'Ed' , age:55, voted:true},
+//      {name:'Tami' , age: 54, voted:true},
+//      {name: 'Mary', age: 31, voted: false},
+//      {name: 'Becky', age: 43, voted: false},
+//      {name: 'Joey', age: 41, voted: true},
+//      {name: 'Jeff', age: 30, voted: true},
+//      {name: 'Zack', age: 19, voted: false}
+//  ];
+//  console.log(totalVotes(voters));
+// function shoppingSpree(arr) {
+//    let x=arr.map(key=>key.price); 
+//     return x
+//  }
+ 
+//  var wishlist = [
+//      { title: "Tesla Model S", price: 90000 },
+//      { title: "4 carat diamond ring", price: 45000 },
+//      { title: "Fancy hacky Sack", price: 5 },
+//      { title: "Gold fidgit spinner", price: 2000 },
+//      { title: "A second Tesla Model S", price: 90000 }
+//  ];
+ 
+//  console.log(shoppingSpree(wishlist)); // 227005
+
+
+// let john = { name: "john", surname: "smith", age: 20 };
+// let pete = { name: "pete", surname: "hunt", age: 20 };
+ 
+// let people = [john, pete];
+// mymap=people.map((user,id)=>{(funame:`${user.name}${user.surname},age:
+// `${use.age}`,id+`${idx+1}`}
+// }));
+
+// let x=1
+// function func() {
+//   console.log(x); // ReferenceError: Cannot access 'x' before initialization
+// //let x = 2;
+// }
+// let arr = [1, 2, 3, 4, 5, 6, 7]
+
+// function aass(arr){
+// return arr.filter(inBetween(3,6));
+// }
+// console.log(aass(arr));
+// let users = [
+//     { name: "John", age: 20, surname: "Johnson" },
+//     { name: "Pete", age: 18, surname: "Peterson" },
+//     { name: "Ann", age: 19, surname: "Hathaway" }
+//   ];
+
+// function sortnamx(arr){
+//     return arr.sort((a, b) => a.name > b.name ? 1 : -1)
+// }
+// console.log(sortnamx(users));
+
+let arr = [1, 2, 3, 4, 5, 6, 7];
+//alert( arr.filter(inArray([1, 2, 10]));
+function abcd(arr){
+    return arr.include()
+}
+
